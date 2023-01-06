4f080d10f673a4fef245c0468913e5b27101a171
Ilnaz <ilnazdaminov@bk.ru>
2023-01-06 15:59:35 +0300

    feature-1/bot created

---
+++ dispatcher/src/main/java/ru/itis/controller/UpdateController.java
@@ -0,0 +1,86 @@
+package ru.itis.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.itis.service.UpdateProducer;
+import ru.itis.utils.MessageUtils;
+
+import static ru.itis.model.RabbitQueue.*;
+
+@Component
+@Slf4j
+public class UpdateController {
+
+    private TelegramBot telegramBot;
+    private MessageUtils messageUtils;
+    private UpdateProducer updateProducer;
+
+    public UpdateController(MessageUtils messageUtils, UpdateProducer updateProducer) {
+        this.messageUtils = messageUtils;
+        this.updateProducer = updateProducer;
+    }
+
+    public void registerBot(TelegramBot telegramBot) {
+        this.telegramBot = telegramBot;
+    }
+
+    public void processUpdate(Update update) {
+        if (update == null) {
+            log.error("Received update is null");
+            return;
+        }
+
+        if (update.getMessage() != null) {
+            distributeMessagesByType(update);
+        } else {
+            log.error("Unsupported message type is received: " + update);
+        }
+    }
+
+    private void distributeMessagesByType(Update update) {
+        Message message = update.getMessage();
+        if (message.getText() != null) {
+            processTextMessage(update);
+        } else if (message.getDocument() != null) {
+            processDocMessage(update);
+        } else if (message.getPhoto() != null) {
+            processPhotoMessage(update);
+        } else {
+            setUnsupportedMessageTypeView(update);
+        }
+    }
+
+    private void setUnsupportedMessageTypeView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Неподдерживаемый тип сообщения");
+        setView(sendMessage);
+    }
+
+    private void setFileIsReceivedView(Update update) {
+        var sendMessage = messageUtils.generateSendMessageWithText(update,
+                "Файл получен. Идет обработка...");
+        setView(sendMessage);
+    }
+
+    private void setView(SendMessage sendMessage) {
+        telegramBot.sendAnswerMessage(sendMessage);
+    }
+
+    private void processDocMessage(Update update) {
+        updateProducer.produce(DOC_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+    private void processTextMessage(Update update) {
+        updateProducer.produce(TEXT_MESSAGE_UPDATE, update);
+    }
+
+    private void processPhotoMessage(Update update) {
+        updateProducer.produce(PHOTO_MESSAGE_UPDATE, update);
+        setFileIsReceivedView(update);
+    }
+
+}
---
+++ dispatcher/src/main/java/ru/itis/service/impl/AnswerConsumerImpl.java
@@ -0,0 +1,15 @@
+package ru.itis.service.impl;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Service;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.itis.service.AnswerConsumer;
+
+@Service
+@Log4j
+public class AnswerConsumerImpl implements AnswerConsumer {
+    @Override
+    public void consume(SendMessage sendMessage) {
+        log.debug(sendMessage);
+    }
+}
