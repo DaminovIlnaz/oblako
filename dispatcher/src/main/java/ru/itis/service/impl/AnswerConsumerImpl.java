fac764ed6e3c7123b8ca7ca199378119fbf1413c
Ilnaz <ilnazdaminov@bk.ru>
2023-01-07 03:52:25 +0300

    feature-3/created node microservice

--- dispatcher/src/main/java/ru/itis/service/impl/AnswerConsumerImpl.java
+++ dispatcher/src/main/java/ru/itis/service/impl/AnswerConsumerImpl.java
@@ -1,15 +1,27 @@
 package ru.itis.service.impl;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import ru.itis.controller.UpdateController;
 import ru.itis.service.AnswerConsumer;
+
+import static ru.itis.model.RabbitQueue.ANSWER_MESSAGE;
 
 @Service
 @Log4j
 public class AnswerConsumerImpl implements AnswerConsumer {
+    private final UpdateController updateController;
+
+    public AnswerConsumerImpl(UpdateController updateController) {
+        this.updateController = updateController;
+    }
+
     @Override
+    @RabbitListener(queues = ANSWER_MESSAGE)
     public void consume(SendMessage sendMessage) {
         log.debug(sendMessage);
+        updateController.setView(sendMessage);
     }
 }
